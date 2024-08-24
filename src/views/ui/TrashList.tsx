7125538fe74ae5ee0aa832995763533aeb34625c
siyoon-jung <siyoon.syj@gmail.com>
2024-08-25 01:26:30 +0900

    feat: 이미지 네이밍 변경

--- src/views/ui/TrashList.tsx
+++ src/views/ui/TrashList.tsx
@@ -2,24 +2,24 @@ import TrashItem from './TrashItem'
 import styles from './TrashList.module.scss'
 import glass from '../../img/glass.png';
 import all from '../../img/all.png';
-import box from '../../img/box.png';
+import paper from '../../img/paper.png';
 import can from '../../img/can.png';
 import etc from '../../img/etc.png';
-import pet from '../../img/pet.png';
 import plastic from '../../img/plastic.png';
-import recycling from '../../img/recycling.png';
+import plastic_bac from '../../img/plastic_bag.png';
+import recycle from '../../img/recycle.png';
 import styrofoam from '../../img/styrofoam.png';
 
 const List = [
-  {id: '1', icon: glass, text: '유리', checked: false},
-  {id: '2', icon: all, text: '모든종류', checked: true},
-  {id: '3', icon: box, text: '종이류', checked: true},
-  {id: '4', icon: can, text: '캔', checked: true},
-  {id: '5', icon: etc, text: '기타', checked: true},
-  {id: '6', icon: pet, text: '플라스틱', checked: true},
-  {id: '7', icon: plastic, text: '비닐', checked: true},
-  {id: '8', icon: recycling, text: '재활용', checked: true},
-  {id: '9', icon: styrofoam, text: '스티로폼', checked: true},
+  {id: '1', icon: all, text: '모든종류', checked: true},
+  {id: '2', icon: recycle, text: '재활용', checked: true},
+  {id: '3', icon: plastic, text: '플라스틱', checked: true},
+  {id: '4', icon: paper, text: '종이류', checked: true},
+  {id: '5', icon: plastic_bac, text: '비닐', checked: true},
+  {id: '6', icon: can, text: '캔', checked: true},
+  {id: '7', icon: glass, text: '유리', checked: false},
+  {id: '8', icon: styrofoam, text: '스티로폼', checked: true},
+  {id: '9', icon: etc, text: '기타', checked: true},
 ]
 
 const TrashList = () => {
