f178de55f11878a9243c7a47b95319c1d3c07e02
siyoon-jung <siyoon.syj@gmail.com>
2024-08-24 21:21:29 +0900

    feat: ui 작업

--- src/views/components/pages/TrashCollect.tsx
+++ src/views/components/pages/TrashCollect.tsx
@@ -1,6 +1,18 @@
+import Button from "../../ui/Button"
+import ButtonWrapper from "../../ui/ButtonWrapper"
+import Header from "../../ui/Header"
+import Map from '../../ui/Map'
+
 const TrashCollect = () => {
   return (
-    <>쓰레기 수집 페이지</>
+    <>
+      <Header />
+      <Map />
+      <ButtonWrapper>
+        <Button title='수집 시작' onClick={()=>{}} />
+        <Button type='blue' title='수집 끝내기' onClick={()=>{}} />      
+    </ButtonWrapper>
+    </>
   )
 }
 
