195cfb58216b056b475602709956879a354311a9
siyoon-jung <siyoon.syj@gmail.com>
2024-08-24 18:08:00 +0900

    feat: ui 오류 커밋

---
+++ src/views/ui/ButtonWrapper.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+
+const ButtonWrapper = () => {
+  return (
+    <div>
+      <Button title='text' />
+    </div>
+  )
+}
+
+export default ButtonWrapper
